a638698cd720694ba57a75f0b14f6369b9223a60
r0dinockiy-frontdevelopier <karintsev.roma545@gmail.com>
2025-12-23 10:04:11 +0300

    Создание страницы Home для пустой ссылки

--- config/articles/urls.py
+++ config/articles/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import ArticleListView, ArticleCreateView, ArticleUpdateView,  ArticleDeleteView
+from .views import ArticleListView, ArticleCreateView, ArticleUpdateView,  ArticleDeleteView, HomeView
 
 urlpatterns = [
-    
+    path('', HomeView.as_view(), name='home'),
     path('articles', ArticleListView.as_view(), name='article_list'),
     path('articles/create', ArticleCreateView.as_view(), name='article_create'),
     path('articles/<int:pk>/edit', ArticleUpdateView.as_view(), name='article_edit'),
--- config/articles/views.py
+++ config/articles/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, TemplateView
 from django.urls import reverse_lazy
 from .models import Article
 
@@ -22,9 +22,11 @@ class ArticleUpdateView(UpdateView):
     template_name = 'articles/article_form.html'
     success_url = reverse_lazy('article_list')
 
-    
+
 class ArticleDeleteView(DeleteView):
     model = Article
     template_name = 'articles/article_confirm_delete.html'
     success_url = reverse_lazy('article_list')
 
+class HomeView(TemplateView):
+    template_name = 'articles/home.html'
