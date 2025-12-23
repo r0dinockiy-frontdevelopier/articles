cac931ee76fa40e050709945a4034cc43452d9b4
r0dinockiy-frontdevelopier <karintsev.roma545@gmail.com>
2025-12-23 08:37:12 +0300

    Создал представления

--- config/articles/views.py
+++ config/articles/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from .models import Article
 
-# Create your views here.
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'articles/article_list.html'
+    context_object_name = 'articles'
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    fields = ['title', 'content']
+    template_name = 'articles/article_form.html'
+    success_url = reverse_lazy('article_list')
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ['title', 'content']
+    template_name = 'articles/article_form.html'
+    success_url = reverse_lazy('article_list')
